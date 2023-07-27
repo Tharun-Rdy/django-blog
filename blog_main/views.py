f9bfd609fcc85d6b725beffbcdb06770f7a6a112
tharun <tharundadithota@gmail.com>
2023-07-27 13:54:13 +0530

    Posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,15 @@
-
 from django.shortcuts import render
+from blogs.models import Blog, Category
 
 def home(request): # when home function triggers then home.html should be executed.
-    return render(request, 'home.html') # render always take the request
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at') # we are using filter bcz we want to add a condition to this query
+    posts = Blog.objects.filter(is_featured=False, status='Published' )
+    # If post is Drafted or not published then we are not supposed to display it to the user.
+    
+    context = {   # we will send the categories to home.html
+        'featured_posts': featured_posts,
+        'posts': posts,
+    }
+    # print statement is used when we want to see the output in the terminal
+    return render(request, 'home.html', context) # render always take the request
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
 
-# Create your views here.
+from .models import Blog, Category
+
+def posts_by_category(request, category_id):
+    # Fetch the posts that belong to the category with the id, category_id
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    # Here we are using the cstegory_id bcz ForeignKey accepts only id but not names
+    # use try/except when we want to do some custom action
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        # redirect the user to the homepage
+        return redirect('home')
+    # Use get_object_or_404 when you want to display 404 error page to the user
+    # category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'posts':posts,
+        'category':category,
+    }
+    return render(request, 'posts_by_category.html', context)
